1c142cd16c80bdbb35b2e25305bf12e3c0369f27
danisteb90 <esteban.alban9003@gmail.com>
2022-06-28 20:26:32 -0500

    Primer commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cliente cliente = new Cliente();
+        cliente.edad = 22;
+        cliente.nombre = "Diego";
+        cliente.telefono = "0956742657";
+        cliente.credito = "180 dias";
+
+        System.out.println("Edad: " + cliente.edad);
+        System.out.println("Nombre: " + cliente.nombre);
+        System.out.println("Telf: " + cliente.telefono);
+        System.out.println("Credito: " + cliente.credito);
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.edad = 35;
+        trabajador.nombre = "Carlos";
+        trabajador.telefono = "0947862116";
+        trabajador.salario = 350;
+
+        System.out.println("Edad: " + trabajador.edad);
+        System.out.println("Nombre: " + trabajador.nombre);
+        System.out.println("Telf: " + trabajador.telefono);
+        System.out.println("Salario: " + trabajador.salario);
     }
+}
+
+class Persona {
+    int edad;
+    String nombre;
+    String telefono;
+}
+
+final class Cliente extends Persona {
+    String credito;
+}
+
+final class Trabajador extends Persona {
+    int salario;
 }
